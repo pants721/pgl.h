065da385023fabea5b20ed8aabab67340e0db4b3
Lucas Newcomb <pantsbabyy@gmail.com>
2023-06-14 19:12:17 -0400

    Basic functionality

--- src/pgl.c
+++ src/pgl.c
@@ -3,9 +3,7 @@
 #include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
-
-#define MIN(a,b) (((a)<(b))?(a):(b))
-#define MAX(a,b) (((a)>(b))?(a):(b))
+#include <stdio.h>
 
 pgl_canvas *pgl_canvas_new(uint32_t *pixels, int width, int height) {
     pgl_canvas *pc = (pgl_canvas *)malloc(sizeof(pgl_canvas));
@@ -16,109 +14,130 @@ pgl_canvas *pgl_canvas_new(uint32_t *pixels, int width, int height) {
 }
 
 void pgl_set_pixel(pgl_canvas *pc, int x, int y, uint32_t color) {
-    pc->pixels[x + (y * pc->width)] = color;
+    if (pgl_in_bounds(pc, x, y)) {
+        pc->pixels[x + (y * pc->width)] = color;
+    }
 }
 
-/* static void _pgl_draw_line_low(pgl_canvas *pc, int x1, int y1, int x2, int y2, */
-/*                                uint32_t color) { */
-/*     int diff_x = x2 - x1; */
-/*     int diff_y = y2 - y1; */
-/*     int y_inc = 1; */
-/*     if (diff_y < 0) { */
-/*         y_inc = -1; */
-/*         diff_y = -diff_y; */
-/*     } */
-/*     int D = (2 * diff_y) - diff_x; */
-/*     int y = y1; */
+void pgl_clear(pgl_canvas *pc) {
+    for (int i = 0; i < pc->width * pc->height; i++) {
+        pc->pixels[i] = NULL;
+    }
+}
 
-/*     for (int x = x1; x <= x2; x++) { */
-/*         pgl_set_pixel(pc, x, y, color); */
-/*         if (D > 0) { */
-/*             y += y_inc; */
-/*             D += (2 * (diff_y - diff_x)); */
-/*         } else { */
-/*             D += 2 * diff_y; */
-/*         } */
-/*     } */
-/* } */
-
-/* static void _pgl_draw_line_high(pgl_canvas *pc, int x1, int y1, int x2, int y2, */
-/*                                 uint32_t color) { */
-/*     int diff_x = x2 - x1; */
-/*     int diff_y = y2 - y1; */
-/*     int x_inc = 1; */
-
-/*     if (diff_x < 0) { */
-/*         x_inc = -1; */
-/*         diff_x = -diff_x; */
-/*     } */
-/*     int D = (2 * diff_x) - diff_y; */
-/*     int x = x1; */
-
-/*     for (int y = y1; y <= y2; y++) { */
-/*         pgl_set_pixel(pc, x, y, color); */
-/*         if (D > 0) { */
-/*             x += x_inc; */
-/*             D += (2 * (diff_x - diff_y)); */
-/*         } else { */
-/*             D += 2 * diff_x; */
-/*         } */
-/*     } */
-/* } */
-
-/* void pgl_draw_line(pgl_canvas *pc, int x1, int y1, int x2, int y2, */
-/*                    uint32_t color) { */
-/*     if (x1 == x2) { */
-/*         for (int i = MIN(y1, y2); i <= MAX(y1, y2); i++) { */
-/*             pgl_set_pixel(pc, x1, i, color); */
-/*         } */
-/*     } else if (y1 == y2) { */
-/*         for (int i = MIN(x1, x2); i <= MAX(x1, x2); i++) { */
-/*             pgl_set_pixel(pc, i, y1, color); */
-/*         } */
-/*     } else { */
-/*         // Diagonal line */
-/*         // Source: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm */
-/*         if (abs(y2 - y1) < abs(x2 - x1)) { */
-/*             if (x1 > x2) { */
-/*                 _pgl_draw_line_low(pc, x2, y2, x1, y1, color); */
-/*             } else { */
-/*                 _pgl_draw_line_low(pc, x1, y1, x2, y2, color); */
-/*             } */
-/*         } else { */
-/*             if (y1 > y2) { */
-/*                 _pgl_draw_line_high(pc, x2, y2, x1, y1, color); */
-/*             } else { */
-/*                 _pgl_draw_line_high(pc, x1, y1, x2, y2, color); */
-/*             } */
-/*         } */
-/*     } */
-/* } */
-
-void pgl_draw_line(pgl_canvas *pc, int x1, int y1, int x2, int y2,
+void pgl_line(pgl_canvas *pc, int x1, int y1, int x2, int y2,
                    uint32_t color) {
-    int diff_x = abs(x1 - x2);
-    int sign_x = x1 < x2 ? 1 : -1;
+    int dx = abs(x1 - x2);
+    int sx = x1 < x2 ? 1 : -1;
 
-    int diff_y = -abs(y2 - y1);
-    int sign_y = y1 < y2 ? 1 : -1;
+    int dy = -abs(y2 - y1);
+    int sy = y1 < y2 ? 1 : -1;
 
-    int error = diff_x + diff_y;
+    int e = dx + dy;
 
     while (true) {
         pgl_set_pixel(pc, x1, y1, color);
         if (x1 == x2 && y1 == y2) { break; }
-        int error2 = 2 * error;
-        if (error2 >= diff_y) {
+        int e2 = 2 * e;
+        if (e2 >= dy) {
             if (x1 == x2) { break; }
-            error += diff_y;
-            x1 += sign_x;
+            e += dy;
+            x1 += sx;
         }
-        if (error2 <= diff_x) {
+        if (e2 <= dx) {
             if (y1 == y2) { break; }
-            error += diff_x;
-            y1 += sign_y;
+            e += dx;
+            y1 += sy;
         }
     }
+}
 
+void pgl_circle(pgl_canvas *pc, int center_x, int center_y, int r, 
+        uint32_t color) {
+    int t1 = r / 16;
+    int x = r;
+    int y = 0;
+    while (x >= y) {
+      pgl_line(pc, center_x + x, center_y + y, center_x - x, center_y + y,
+               color);
+      pgl_line(pc, center_x + x, center_y - y, center_x - x, center_y - y,
+               color);
+      pgl_line(pc, center_x + y, center_y + x, center_x - y, center_y + x,
+               color);
+      pgl_line(pc, center_x + y, center_y - x, center_x - y, center_y - x,
+               color);
+      y += 1;
+      t1 += y;
+      int t2 = t1 - x;
+      if (t2 >= 0) {
+        t1 = t2;
+        x -= 1;
+        }
+    }
+}
+
+inline static void bottom_flat_triangle(pgl_canvas *pc, int x1, int y1, int x2, int y2, int x3, int y3, uint32_t color) {
+    float inv_slope_1 = (x2 - x1) / (y2 - y1);
+    float inv_slope_2 = (x3 - x1) / (y3 - y1);
+
+    float curr_x1 = x1;
+    float curr_x2 = x1;
+
+    for (int scan_y = y1; scan_y <= y2; scan_y++) {
+        pgl_line(pc, (int)curr_x1, scan_y, (int)curr_x2, scan_y, color);
+        curr_x1 += inv_slope_1;
+        curr_x2 += inv_slope_2;
+    }
+}
+
+inline static void top_flat_triangle(pgl_canvas *pc, int x1, int y1, int x2, int y2, int x3, int y3, uint32_t color) {
+    float inv_slope_1 = (x3 - x1) / (y3 - y1);
+    float inv_slope_2 = (x3 - x2) / (y3 - y2);
+
+    float curr_x1 = x3;
+    float curr_x2 = x3;
+
+    for (int scan_y = y3; scan_y <= y1; scan_y--) {  
+        pgl_line(pc, (int)curr_x1, scan_y, (int)curr_x2, scan_y, color);
+        curr_x1 -= inv_slope_1;
+        curr_x2 -= inv_slope_2;
+    }
+}
+
+void pgl_triangle(pgl_canvas *pc, int x1, int y1, int x2, int y2, int x3, int y3, uint32_t color) {
+    int x_temp;
+    int y_temp;
+
+    if (y1 > y2) {
+        x_temp = x1; y_temp = y1;
+        x1 = x2; y1 = y2;
+        x2 = x_temp; y2 = y_temp;
+    }
+    
+    if (y1 > y3) {
+        x_temp = x1; y_temp = y1;
+        x1 = x3; y1 = y3;
+        x3 = x_temp; y3 = y_temp;
+    }
+
+    if (y2 > y3) {
+        x_temp = x2; y_temp = y2;
+        x2 = x3; y2 = y3;
+        x3 = x_temp; y3 = y_temp;
+    }
+
+    if (y2 == y3) {
+        bottom_flat_triangle(pc, x1, y1, x2, y2, x3, y3, color);
+    } else if (y1 == y2) {
+        top_flat_triangle(pc, x1, y1, x2, y2, x3, y3, color);
+    } else {
+        int x4 = (int)(x1 + (float)(y2 - y1) / (float)(y3 - y1) * (x3 - x1));
+        int y4 = y2;
+        bottom_flat_triangle(pc, x1, y1, x2, y2, x4, y4, color);
+        top_flat_triangle(pc, x2, y2, x4, y4, x3, y3, color);
+    }
+}
+
+bool pgl_in_bounds(pgl_canvas *pc, int x, int y) {
+    return 0 <= x && x < (int)pc->width && 0 <= y && y < (int)pc->height;
 }
--- src/pgl.h
+++ src/pgl.h
@@ -1,16 +1,39 @@
 #ifndef PGL_H
 #define PGL_H
 
+#include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-typedef struct {
+#define PGL_MIN(a,b) (((a)<(b))?(a):(b))
+#define PGL_MAX(a,b) (((a)>(b))?(a):(b))
+
+// Colors
+#define PGL_RGBA(r, g, b, a) ((((r)&0xFF)<<(8*0)) | (((g)&0xFF)<<(8*1)) | (((b)&0xFF)<<(8*2)) | (((a)&0xFF)<<(8*3)))
+#define PGL_BLACK    PGL_RGBA(0, 0, 0, 1)
+#define PGL_RED      PGL_RGBA(255, 0, 0, 1)
+#define PGL_GREEN    PGL_RGBA(0, 255, 0, 1)
+#define PGL_BLUE     PGL_RGBA(0, 0, 255, 1)
+#define PGL_YELLOW   PGL_RGBA(255, 255, 0, 1)
+#define PGL_MAGENTA  PGL_RGBA(255, 0, 255, 1)
+#define PGL_CYAN     PGL_RGBA(0, 255, 255, 1)
+#define PGL_SILVER   PGL_RGBA(192, 192, 192, 1)
+#define PGL_GRAY     PGL_RGBA(128, 128, 128, 1)
+#define PGL_DARKGRAY PGL_RGBA(64, 64, 64, 1)
+
+
+typedef struct pgl_canvas {
     uint32_t *pixels;
     int width, height;
 } pgl_canvas;
 
 pgl_canvas *pgl_canvas_new(uint32_t *pixels, int width, int height);
 void pgl_set_pixel(pgl_canvas *pc, int x, int y, uint32_t color);
-void pgl_draw_line(pgl_canvas *pc, int x1, int y1, int x2, int y2,
-                   uint32_t color);
+void pgl_clear(pgl_canvas *pc);
+void pgl_line(pgl_canvas *pc, int x1, int y1, int x2, int y2, uint32_t color);
+void pgl_circle(pgl_canvas *pc, int center_x, int center_y, int r, uint32_t color);
+void pgl_triangle(pgl_canvas *pc, int x1, int y1, int x2, int y2, int x3, int y3, uint32_t color);
+
+bool pgl_in_bounds(pgl_canvas *pc, int x, int y);
 
 #endif // PGL_H
