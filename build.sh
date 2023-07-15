eba57d415bc91d240f11e75f566d61b71ab36bc9
pants721 <lucasnewcomb721@gmail.com>
2023-07-15 02:36:51 -0400

    Made build.sh more generic

--- build.sh
+++ build.sh
@@ -7,9 +7,10 @@ PKGS="sdl2"
 CFLAGS="-Wall -Wextra -std=c2x -pedantic -ggdb -O3"
 LIBS=-lm
 SRC="demo.c"
+OUT="out"
 
 if [ `uname` = "Darwin" ]; then
     CFLAGS+=" -framework OpenGL"
 fi
 
-$CC $CFLAGS `pkg-config --cflags $PKGS` -o pgl $SRC $LIBS `pkg-config --libs $PKGS`
+$CC $CFLAGS `pkg-config --cflags $PKGS` -o $OUT $SRC $LIBS `pkg-config --libs $PKGS`
